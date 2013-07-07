a22fdfd1fb7938164a2c47f43007544d94c8ea8d
Grégory <Gregory.Mounie@imag.fr>
2013-07-08 00:02:45 +0200

    debut portage vers gtest

--- tests/test_generic.cc
+++ tests/test_generic.cc
@@ -7,129 +7,139 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 
-#include <CUnit/CUnit.h>
+#include <gtest/gtest.h>
 
 #include "mem.h"
 #include "test_run_cpp.h"
 #include "test_generic.h"
 
-void test_init()
-{
-  int i;
-
-  CU_ASSERT_PTR_NULL_FATAL( mem_alloc(64) );
-
-  CU_ASSERT_EQUAL_FATAL( mem_init(), 0 );
-  CU_ASSERT_EQUAL_FATAL( mem_destroy(), 0);
-
-  for(i=0; i< 100; i++)
-    {
-      void *m1=0;
-      CU_ASSERT_EQUAL_FATAL( mem_init(), 0 );
-      CU_ASSERT_PTR_NOT_NULL_FATAL( m1  = mem_alloc(64) );
-      memset(m1, i, 64);
-      CU_ASSERT_EQUAL_FATAL( mem_free(m1, 64), 0 );
-      CU_ASSERT_EQUAL_FATAL( mem_destroy(), 0);
-    }
+TEST(Init, noinit) {
+  ASSERT_EQ( mem_alloc(64), 0 );
 }
 
-void test_alloc_free()
-{
-  void *m1=0, *m2=0;
+TEST(Init, initdestroy) {
+  ASSERT_EQ(  mem_init(), 0 );
+  ASSERT_EQ( mem_destroy(), 0);
+}
 
-  CU_ASSERT_EQUAL_FATAL( mem_init(), 0);
-  CU_ASSERT_PTR_NULL_FATAL( mem_alloc(0) );  
+TEST(Init, oneallocloop) {
+  for(int i=0; i< 100; i++) {
+    void *m1=0;
+    ASSERT_EQ( mem_init(), 0 );
+    m1  = mem_alloc(64);
+    ASSERT_NE( m1, 0 );
+    memset(m1, i, 64);
+    ASSERT_EQ( mem_free(m1, 64), 0 );
+    ASSERT_EQ( mem_destroy(), 0);
+  } 
+}
 
-  CU_ASSERT_PTR_NOT_NULL_FATAL( m1 = mem_alloc(ALLOC_MEM_SIZE) );
+class BaseMemTest : public ::testing::Test {
+public:
+  virtual void SetUp() {
+    ASSERT_EQ(  mem_init(), 0 );
+  }
+  virtual void TearDown() {
+    ASSERT_EQ( mem_destroy(), 0);
+  }
+}
+
+
+TEST_F(BaseMemTest, allocnone) {
+  ASSERT_EQ( mem_alloc(0), 0);
+}
+
+TEST_F(BaseMemTest, allocall) {
+  void * m1 = mem_alloc(ALLOC_MEM_SIZE);
+  ASSERT_NE( m1, 0 );
   memset(m1, 1, ALLOC_MEM_SIZE);
-  CU_ASSERT_PTR_NULL_FATAL( mem_alloc(1) );
-  CU_ASSERT_EQUAL_FATAL( mem_free(m1, ALLOC_MEM_SIZE), 0 );
-
-  CU_ASSERT_PTR_NOT_NULL_FATAL( m2 = mem_alloc(ALLOC_MEM_SIZE) );
+  void * m2 =  mem_alloc(1);
+  ASSERT_EQ( m2, 0 );
+  ASSERT_EQ(mem_free(m1, ALLOC_MEM_SIZE), 0 );
+  void * m3 = mem_alloc(ALLOC_MEM_SIZE);
+  ASSERT_EQ( m1, m3);
   memset(m1, 2, ALLOC_MEM_SIZE);
-  CU_ASSERT_PTR_EQUAL_FATAL( m1, m2 );
-  CU_ASSERT_EQUAL_FATAL( mem_free(m2, ALLOC_MEM_SIZE), 0 );
-
-  CU_ASSERT_EQUAL_FATAL( mem_destroy(), 0);
+  ASSERT_EQ( mem_free(m2, ALLOC_MEM_SIZE), 0 );
 }
 
-void test_free_boundaries()
-{
-  void *m1=0, *m2=0;
+TEST_F(BaseMemTest, boundaries) { 
+  void * m1=  mem_alloc(ALLOC_MEM_SIZE);
+  ASSERT_NE( m1, 0 );
+  ASSERT_EQ( mem_free(m1, ALLOC_MEM_SIZE), 0 );
   
-  CU_ASSERT_EQUAL_FATAL( mem_init(), 0 );
-  CU_ASSERT_PTR_NOT_NULL_FATAL( m1 = mem_alloc(ALLOC_MEM_SIZE) );
-  CU_ASSERT_EQUAL_FATAL( mem_free(m1, ALLOC_MEM_SIZE), 0 );
-	  
-  CU_ASSERT_NOT_EQUAL_FATAL( mem_free( (void *)-1, ALLOC_MEM_SIZE), 0 );
-  m2 =  m1 - 1 ;
-  CU_ASSERT_NOT_EQUAL_FATAL( mem_free( m2, ALLOC_MEM_SIZE), 0 );
-  m2 =  m1 + ALLOC_MEM_SIZE + 1;
-  CU_ASSERT_NOT_EQUAL_FATAL( mem_free( m2, ALLOC_MEM_SIZE), 0 );
-
-  CU_ASSERT_EQUAL_FATAL( mem_destroy(), 0);
+  ASSERT_NE( mem_free( (void *)-1, ALLOC_MEM_SIZE), 0 );
+  void *m2= m1 -1;
+  ASSERT_NE( mem_free( m2, ALLOC_MEM_SIZE), 0 );
+  void *m3= m1 + ALLOC_MEM_SIZE + 1;
+  ASSERT_NE( mem_free( m3, ALLOC_MEM_SIZE), 0 );
 }
 
-void test_bound()
-{
-  void *mref=0, *m1=0, *m2=0;
-
-  CU_ASSERT_EQUAL_FATAL( mem_init(), 0 );
-
-  CU_ASSERT_PTR_NOT_NULL_FATAL( mref = mem_alloc(ALLOC_MEM_SIZE) );
+TEST_F(BaseMemTest, intervals) {
+  void *mref = mem_alloc(ALLOC_MEM_SIZE);
+  ASSERT_NE( mref, 0);
   memset(mref, 6, ALLOC_MEM_SIZE);
-  CU_ASSERT_EQUAL_FATAL( mem_free( mref, ALLOC_MEM_SIZE ), 0 );
+  ASSERT_EQ( mem_free( mref, ALLOC_MEM_SIZE ), 0 );
 
-  CU_ASSERT_PTR_NOT_NULL_FATAL( m1 = mem_alloc(64) );
+  void *m1 = mem_alloc(64);
+  ASSERT_NE( m1, 0 );
   memset(m1, 6, 64);
-  CU_ASSERT_PTR_NOT_NULL_FATAL( m2 = mem_alloc(64) );
+
+  void *m2 = mem_alloc(64);
+  ASSERT_NE( m2, 0 );
   memset(m2, 6, 64);
 
-  CU_ASSERT_EQUAL_FATAL( mem_free( m1, 64 ), 0 );
-  CU_ASSERT_EQUAL_FATAL( mem_free( m2, 64 ), 0 );
+  ASSERT_GE( m1, mref );
+  ASSERT_LT( m1, mref+ALLOC_MEM_SIZE );
+  ASSERT_GE( m2, mref );
+  ASSERT_LT( m2, mref+ALLOC_MEM_SIZE );
 
-  CU_ASSERT_FATAL( m1 >= mref );
-  CU_ASSERT_FATAL( m1 < mref+ALLOC_MEM_SIZE );
-  CU_ASSERT_FATAL( m2 >= mref );
-  CU_ASSERT_FATAL( m2 < mref+ALLOC_MEM_SIZE );
-      
-  CU_ASSERT_EQUAL_FATAL( mem_destroy(), 0);
-
+  ASSERT_EQ( mem_free( m1, 64 ), 0 );
+  ASSERT_EQ( mem_free( m2, 64 ), 0 );
 }
 
-void test_alloc_free_list()
-{
+TEST_F(BaseMemTest, bouclefreepairimpair) {
   int nb=2*50;
   void *tab[nb];
-  int i;
 
-  CU_ASSERT( 64*nb < ALLOC_MEM_SIZE );
- 
-  CU_ASSERT_EQUAL_FATAL( mem_init(), 0 );
- 
-  for(i=0; i < nb; i++)
+  ASSERT_LT( 64*nb,  ALLOC_MEM_SIZE );
+  
+  for(int i=0; i < nb; i++)
     {
-      CU_ASSERT_PTR_NOT_NULL_FATAL( tab[i] = mem_alloc(64) );
+      tab[i] = mem_alloc(64);
+      ASSERT_NE( tab[i], 0 );
       memset(tab[i], 3, 64);
     }
-
-  for(i=0; i < nb; i+=2)
+  for(int i=0; i < nb; i+=2)
     {
-      CU_ASSERT_EQUAL_FATAL( mem_free( tab[i], 64 ), 0 );
+      ASSERT_EQ( mem_free( tab[i], 64 ), 0 );
+    }
+  for(int i=1; i < nb; i+=2)
+    {
+      ASSERT_EQ( mem_free( tab[i], 64 ), 0 );
     }
 
-  for(i=1; i < nb; i+=2)
-    {
-      CU_ASSERT_EQUAL_FATAL( mem_free( tab[i], 64 ), 0 );
-    }
-
-  CU_ASSERT_PTR_NOT_NULL_FATAL( tab[0] = mem_alloc(ALLOC_MEM_SIZE) );
-  memset(tab[0], 4, ALLOC_MEM_SIZE);
-  CU_ASSERT_EQUAL_FATAL( mem_free( tab[0], ALLOC_MEM_SIZE ), 0 );
-      
-  CU_ASSERT_EQUAL_FATAL( mem_destroy(), 0);
-  
+  void *m1 = mem_alloc(ALLOC_MEM_SIZE);
+  ASSERT_NE( m1, 0 );
+  memset(m1, 4, ALLOC_MEM_SIZE);
+  ASSERT_EQ( mem_free( m1, ALLOC_MEM_SIZE ), 0 );
 }
+
+TEST_F(BaseMemTest, petitetaille) {
+  int multi= 0;
+
+#ifdef BUDDY
+  multi=1;
+#endif
+#ifdef CFF
+  multi=2;
+#endif
+#ifdef BF
+  multi=2;
+#endif
+
+  ASSERT_GT(multi, 0);
+
+  
 
 void test_small()
 {
